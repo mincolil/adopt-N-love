ae03bea192de640ef00f6e8312267a7c9a1755b7
minhdlhe163242 <minhdlhe163242@fpt.edu.vn>
2024-01-23 22:25:20 +0700

    api getall adopt + create adopt

--- backend/routes/adoptRoutes.js
+++ backend/routes/adoptRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const adoptController = require('../controllers/adoptController');
+const userController = require('../controllers/usersController');
 
 
-router.get('/', (res, req) => { res.send('Hello World') })
+router.get('/', adoptController.getAllAdopt)
     .post('/create-new-adopt', adoptController.createNewAdopt)
 
 module.exports = router;

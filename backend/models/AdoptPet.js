4685d6f0ed8c08d0a603248af19991d1dc12c7c8
minhdlhe163242 <minhdlhe163242@fpt.edu.vn>
2024-01-23 21:00:29 +0700

    add adopt pet model

---
+++ backend/models/AdoptPet.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate');
+
+const adoptSchema = new mongoose.Schema({
+    //userId, petName, status, weight, height, petImage, vaccine
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    petName: String,
+    status: {
+        type: Boolean,
+        default: false,
+    },
+    weight: Number,
+    height: Number,
+    petImage: String,
+    vaccine: {
+        type: Boolean,
+        default: false,
+    }
+})
+
+adoptSchema.plugin(mongoosePaginate)
+module.exports = mongoose.model("AdoptPet", adoptSchema)

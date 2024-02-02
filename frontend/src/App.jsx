5286985eeecaf5a3b9f03702c1f9957f2fbea287
HieuNM21 <nmhieu21@gmail.com>
2024-02-02 17:17:10 +0700

    update new Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -115,19 +115,6 @@ function App() {
             </Route>
           </Routes>
         </BrowserRouter>
-
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
       </LocalizationProvider>
     </>
   );

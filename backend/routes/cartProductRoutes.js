ecc5e3e24b61b6f441acf3469156a3c087b758da
minhdlhe163242 <minhdlhe163242@fpt.edu.vn>
2024-02-21 15:03:23 +0700

    smpt mail

--- backend/routes/cartProductRoutes.js
+++ backend/routes/cartProductRoutes.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 const cartProductController = require('../controllers/cartProductController')
 
-router.get('/view-cart', cartProductController.viewCart)
-.post('/checkout', cartProductController.checkout)
-.post('/add-to-cart', cartProductController.addToCart)
-.delete('/remove-from-cart/:productId', cartProductController.removeFromCart)
-   
+router.get('/view-cart', cartProductController.viewCart) //test
+    .post('/checkout', cartProductController.checkout) //test
+    .post('/add-to-cart', cartProductController.addToCart) //test
+    .delete('/remove-from-cart/:productId', cartProductController.removeFromCart)
+
 module.exports = router   
